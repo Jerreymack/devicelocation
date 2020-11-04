4475b168aea15b70df5d867a3342fa108564d1f5
ben-amara-hamza <hamza.ben-amara-extern@renault.com>
2020-11-04 14:43:41 +0100

    update take IMEI value from URL params

--- dist/script.js
+++ dist/script.js
@@ -1,6 +1,22 @@
-(function($b) {
-  window.IMEI = $('.btn-location').html();
+var getUrlParameter = function getUrlParameter(sParam) {
+  var sPageURL = window.location.search.substring(1),
+      sURLVariables = sPageURL.split('&'),
+      sParameterName,
+      i;
 
+  for (i = 0; i < sURLVariables.length; i++) {
+      sParameterName = sURLVariables[i].split('=');
+
+      if (sParameterName[0] === sParam) {
+          return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+      }
+  }
+};
+
+(function($b) {
+  //window.IMEI = $('.btn-location').html();
+  window.IMEI = getUrlParameter('imei');
+  console.log('window.IMEI > ', window.IMEI)
     /*
      (function() {
     var imei = "355757082511888"
